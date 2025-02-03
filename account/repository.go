59ef57ab6209950a492671b1470b1d073de63f77
SIDDHESH RAJENDRA NIMBALKAR <nimbalkarsiddhesh29@gmail.com>
2025-02-03 21:53:26 +0530

    add

---
+++ account/repository.go
@@ -0,0 +1,38 @@
+package account
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Repository interface {
+	Close()
+	PutAccount(ctx context.Context, a Account) error
+	GetAccountById(ctx context.Context, id string) (*Account, error)
+	ListAccounts(ctx context.Context) ([]Account, error)
+}
+
+type postgresRepository struct {
+	db *sql.DB
+}
+
+func NewPostgresRepository(url string) (Repository, error) {
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return &postgresRepository{db}, nil
+}
+
+func (r *postgresRepository) Close() {
+	r.db.Close()
+}
+
+func (r *postgresRepository) Ping() error {
+	return r.db.Ping()
+}
